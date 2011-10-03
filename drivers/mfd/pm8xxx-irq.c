RonGokhale/codeaurora_kernel

mfd: pm8xxx-irq: Don't request for IRQ if devirq is 0
Add check if PMIC devirq is set to valid value or not.

Change-Id: I404755cf5abe81688dce974daf229758888b5818
Signed-off-by: Jay Chokshi <jchokshi@codeaurora.org>

drivers/mfd/pm8xxx-irq.c
@@ -429,13 +429,17 @@ struct pm_irq_chip *  __devinit pm8xxx_irq_init(struct device *dev,
 #endif
 	}
 
-	rc = request_irq(devirq, pm8xxx_irq_handler, pdata->irq_trigger_flag,
+	if (devirq != 0) {
+		rc = request_irq(devirq, pm8xxx_irq_handler,
+				pdata->irq_trigger_flag,
 				"pm8xxx_usr_irq", chip);
-	if (rc) {
-		pr_err("failed to request_irq for %d rc=%d\n", devirq, rc);
-		goto err;
+		if (rc) {
+			pr_err("failed to request_irq for %d rc=%d\n",
+								devirq, rc);
+			goto err;
+		}
+		set_irq_wake(devirq, 1);
 	}
-	set_irq_wake(devirq, 1);
 
 	chip->sys_dev.id = 0;
 	chip->sys_dev.cls = &pm8xxx_irq_class;

AliceOps/android_kernel_samsung_msm8660-common

Conflicts:
	drivers/mfd/pm8xxx-irq.c

drivers/mfd/pm8xxx-irq.c
@@ -404,13 +404,17 @@ struct pm_irq_chip *  __devinit pm8xxx_irq_init(struct device *dev,
 #endif
 	}
 
-	rc = request_irq(devirq, pm8xxx_irq_handler, pdata->irq_trigger_flag,
+	if (devirq != 0) {
+		rc = request_irq(devirq, pm8xxx_irq_handler,
+				pdata->irq_trigger_flag,
 				"pm8xxx_usr_irq", chip);
-	if (rc) {
-		pr_err("failed to request_irq for %d rc=%d\n", devirq, rc);
-		return ERR_PTR(rc);
+		if (rc) {
+			pr_err("failed to request_irq for %d rc=%d\n",
+								devirq, rc);
+		} else {
+			irq_set_irq_wake(devirq, 1);
+		}
 	}
-	irq_set_irq_wake(devirq, 1);
 
 	return chip;
 }